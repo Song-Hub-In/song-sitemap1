04cbeeb8bcbf134be0fa8ba00115d36ce416877e
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-11 12:32:39 +0000

    sitemap/0.txt

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 pip install bs4 lxml
 python3 main.py
-for FILE in sitemap/*; do echo python3 code.py "$FILE"; git add -A --verbose; git commit -m "test"; git push; done
+for FILE in sitemap/*; do echo python3 code.py "$FILE"; git add -A --verbose; git commit -m "$FILE"; git push; done
 
