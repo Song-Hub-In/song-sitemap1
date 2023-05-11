1045c1981f7b071ebcc3e63a7db9be009dcec2ff
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-11 12:21:39 +0000

    test

--- code.py
+++ code.py
@@ -1,28 +1,29 @@
 from __future__ import division
-import sys
-import requests
-from bs4 import BeautifulSoup
 from multiprocessing import Pool
+from bs4 import BeautifulSoup
+from requests import get
+from json import loads
+import sys
 
-wunder = requests.get("https://www.hungama.com/sitemap/song/song-sitemap1.xml")
-parcala = BeautifulSoup(wunder.content, "xml")
-urls_from_xml = []
-loc_tags = parcala.find_all('loc')
-for loc in loc_tags:
-    urls_from_xml.append(loc.get_text().split("/")[5])
-   
-#print(urls_from_xml)
 
 def MidSongLink(song_id):
-    wunder = requests.get("https://curls.api.hungama.com/v1/content/"+song_id+"/url/playable?contentType=4&alang=en&mlang=en&vlang=ta&device=web&platform=a&storeId=1&uid=1036627096")
-    url = wunder.json()["data"]["body"]["data"]["url"]["playable"][2]["data"]
-    r = requests.get(url, allow_redirects=True)
-    open("mid/"+song_id+'.mp3', 'wb').write(r.content)
-#MidSongLink("48199199")
+    try:
+        wunder = get("https://curls.api.hungama.com/v1/content/"+song_id+"/url/playable?contentType=4&alang=en&mlang=en&vlang=ta&device=web&platform=a&storeId=1&uid=1036627096", timeout=10)
+        open("json/"+song_id+'.json', 'wb').write(wunder.content)
 
-p = Pool(70)
-# p.map(handle, songId)
-num_tasks = len(urls_from_xml)
+        url = wunder.json()["data"]["body"]["data"]["url"]["playable"][2]["data"]
+
+        r = get(url, allow_redirects=True)
+        open("mid/"+song_id+'.mp3', 'wb').write(r.content)
+    except:
+        pass
+with open(sys.argv[1], "r") as file1:
+    song_id = file1.readlines()
+
+song_id = [i.strip() for i in song_id]
+
+p = Pool(50)
+num_tasks = len(song_id)
 print(num_tasks)
-for i, _ in enumerate(p.imap_unordered(MidSongLink, urls_from_xml), 1):
+for i, _ in enumerate(p.imap_unordered(MidSongLink, song_id), 1):
     sys.stderr.write('\rdone {0:%}'.format(i/num_tasks))
---
+++ main.sh
@@ -0,0 +1,5 @@
+# pip install bs4 lxml
+# python3 main.py
+# for FILE in sitemap/*; do echo $FILE; done
+# python3 code.py "sitemap/5.txt"
+git add -A --verbose
