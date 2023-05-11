6eb0eaeea837459692c314496851bba443897ba5
Venkatesh S <76198059+Venkatesh-KCET@users.noreply.github.com>
2023-05-11 12:25:41 +0000

    Shell Commend Changed

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
-# pip install bs4 lxml
-# python3 main.py
-# for FILE in sitemap/*; do echo $FILE; done
-# python3 code.py "sitemap/5.txt"
-git add -A --verbose
+pip install bs4 lxml
+python3 main.py
+for FILE in sitemap/*; do echo python3 code.py "$FILE"; git add -A --verbose; git commit -m "test"; git push; done
+
