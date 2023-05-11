eebde4ad7cc8b24705986e925a63e808b097a5ae
helpercorner <89002748+helpercorner@users.noreply.github.com>
2023-05-11 12:42:51 +0000

    sitemap/*

--- code.py
+++ code.py
@@ -4,19 +4,22 @@ from bs4 import BeautifulSoup
 from requests import get
 from json import loads
 import sys
+import os.path
 
 
 def MidSongLink(song_id):
-    try:
-        wunder = get("https://curls.api.hungama.com/v1/content/"+song_id+"/url/playable?contentType=4&alang=en&mlang=en&vlang=ta&device=web&platform=a&storeId=1&uid=1036627096", timeout=10)
-        open("json/"+song_id+'.json', 'wb').write(wunder.content)
+    check_file = os.path.isfile("mid/"+song_id+'.mp3')
+    if(not check_file):
+        try:
+            wunder = get("https://curls.api.hungama.com/v1/content/"+song_id+"/url/playable?contentType=4&alang=en&mlang=en&vlang=ta&device=web&platform=a&storeId=1&uid=1036627096", timeout=10)
+            open("json/"+song_id+'.json', 'wb').write(wunder.content)
 
-        url = wunder.json()["data"]["body"]["data"]["url"]["playable"][2]["data"]
+            url = wunder.json()["data"]["body"]["data"]["url"]["playable"][2]["data"]
 
-        r = get(url, allow_redirects=True)
-        open("mid/"+song_id+'.mp3', 'wb').write(r.content)
-    except:
-        pass
+            r = get(url, allow_redirects=True)
+            open("mid/"+song_id+'.mp3', 'wb').write(r.content)
+        except:
+            pass
 with open(sys.argv[1], "r") as file1:
     song_id = file1.readlines()
 
